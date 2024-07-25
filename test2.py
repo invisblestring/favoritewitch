63c7355d11ebb3a16d028c98fbad1296386813f8
invisblestring <100006482+invisblestring@users.noreply.github.com>
2024-07-25 10:39:07 +0300

    Update test2.py

--- test2.py
+++ test2.py
@@ -16,7 +16,7 @@ def load_lottiefile(url):
         return None
 
 # GitHub raw URL for the file
-github_raw_url = "https://github.com/invisblestring/favoritewitch/blob/0721feac74c05d783460c8b480974d623a8b3e06/Main_Scene3.json"
+github_raw_url = "https://github.com/invisblestring/favoritewitch/blob/main/Main_Scene3.json"
 
 
 
